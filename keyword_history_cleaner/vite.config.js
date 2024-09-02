ff670bb1ce0dd732ed9f63d2e3311b77dcf58cce
milkeclair <milkeclair.black@gmail.com>
2024-09-02 11:49:28 +0900

    feat: init後にviteの設定を変更しただけ

---
+++ keyword_history_cleaner/vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { crx, defineManifest } from "@crxjs/vite-plugin";
+
+const manifest = defineManifest({
+  manifest_version: 3,
+  name: "Keyword History Cleaner",
+  description: "chromeの検索履歴をキーワード検索で削除する",
+  version: "1.0.0",
+  permissions: ["history"],
+  action: {
+    default_popup: "popup.html",
+  },
+});
+
+export default defineConfig({
+  plugins: [react(), crx({ manifest })],
+});
